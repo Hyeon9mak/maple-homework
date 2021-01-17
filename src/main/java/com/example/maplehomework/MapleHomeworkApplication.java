3a46e2cd8ba5d9c61d46a1dacfe5cfb851a24b44
Hyeon9mak <jinha3507@gmail.com>
2021-01-17 15:26:03 +0900

    chore: fix gitignore

---
+++ src/main/java/com/example/maplehomework/MapleHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.maplehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/maplehomework/controller/TestController.java
@@ -0,0 +1,5 @@
+package com.example.maplehomework.controller;
+
+public class TestController {
+
+}
